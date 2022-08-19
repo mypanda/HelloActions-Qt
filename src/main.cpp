5f475ebff5af6a4c2ae9ddbb9b99e8734b9d3701
mypanda <951788234@qq.com>
2022-08-19 18:29:51 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QWebView>
+#include <QMainWindow>
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +13,12 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+    
+    QMainWindow window;
+    QWebView view(&window);
+    view.setGeometry(0, 0, 600, 400);
+    view.setUrl(QUrl("https://github.com/Mr-Phoebe"));
+    window.show();
 
     return app.exec();
 }
